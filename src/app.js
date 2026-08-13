4850128344d33065a75046b98ab75363c3c9ba3e
saiteja <saiteja.pamudurthy@gmail.com>
2026-08-13 19:27:40 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,16 +2,27 @@ const express = require("express");
 
 const app = express();
 
+app.use("/user", (req, res) => {
+  res.send("Hello from user");
+});
+
+// This will only handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Phani", lastName: "Pamudurthy" });
+});
+
+app.post("/user", (req, res) => {
+  //saving  data to database
+  res.send("Data sucessfully saved to database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+
+//this will match all the HTTP method API calls to /tests
 app.use("/test", (req, res) => {
   res.send("Hello from server");
-});
-
-app.use("/profile", (req, res) => {
-  res.send("Hello babu from profile");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello from dashboard");
 });
 
 app.listen(3000, () => {
