6f3ab2265d8fff2ab5c8a42f164bdbcf7366eb3a
saiteja <saiteja.pamudurthy@gmail.com>
2026-08-17 18:18:28 +0530

    get API's

--- src/app.js
+++ src/app.js
@@ -18,6 +18,31 @@ app.post("/signup", async (req, res) => {
   console.log(req.body);
 });
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.find({ emailId: userEmail });
+    if (user.length === 0) {
+      return res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+//Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("database connection established successfully");
