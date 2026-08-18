49ba0b24127fe3498db0e6b60cde38601fe85dc9
saiteja <saiteja.pamudurthy@gmail.com>
2026-08-18 18:02:30 +0530

    API validation

--- src/app.js
+++ src/app.js
@@ -55,11 +55,27 @@ app.delete("/user", async (req, res) => {
 });
 
 //update  data of the user
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
+
   try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k),
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("update not allowed");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("skills cannot be more than 10");
+    }
+
     await User.findByIdAndUpdate(data.userId, data);
+
     res.send("User updated successfully");
   } catch (err) {
     res.status(400).send("something went wrong");
