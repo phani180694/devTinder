95e7c437ad4c85587d2b21f61beb750af7e8117c
saiteja <saiteja.pamudurthy@gmail.com>
2026-08-20 16:10:46 +0530

    created auth middleware

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { validateSignUpData } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -56,23 +57,9 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.get("/profile", async (req, res) => {
+app.get("/profile", userAuth, async (req, res) => {
   try {
-    const cookies = req.cookies;
-
-    const { token } = cookies;
-
-    if (!token) {
-      throw new Error("Invalid Token");
-    }
-    const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
-    const { _id } = decodedMessage;
-
-    const user = await User.findById(_id);
-
-    if (!user) {
-      throw new Error("User does not exist");
-    }
+    const user = req.user;
     res.send(user);
   } catch (err) {
     res.status(400).send("ERROR:" + err.message);
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,22 +1,22 @@
-const adminAuth = (req, res, next) => {
-  console.log("Admin Auth isgetting checked!!");
-  const token = "xyz";
-  const isAdminAuthorized = token === "xyz";
-  if (!isAdminAuthorized) {
-    return res.status(401).send("unauthorized request");
-  } else {
-    next();
-  }
-};
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-const userAuth = (req, res, next) => {
-  console.log("User Auth is getting checked!!");
-  const token = "xyzabc";
-  const isUserAuthorized = token === "xyz";
-  if (!isUserAuthorized) {
-    return res.status(401).send("unauthorized request");
-  } else {
+const userAuth = async (req, res, next) => {
+  try {
+    //read the token from the req cookies
+    const { token } = req.cookies;
+    if (!token) {
+      throw new Error("Token is not valid!!");
+    }
+
+    const decodedObj = await jwt.verify(token, "DEV@Tinder$790");
+
+    const { _id } = decodedObj;
+    const user = await User.findById(_id);
+    req.user = user;
     next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
 };
-module.exports = { userAuth, adminAuth };
+module.exports = { userAuth };
